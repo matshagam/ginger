686881f49bf67b8119f9471b97df0ade6570f881
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-08-04 20:54:59 +0300

    Добавил анимацию на иконки и в навигацию

--- src/components/Cover/Background.jsx
+++ src/components/Cover/Background.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default class Background extends React.Component {
   render() {
-    return <div className="cover-bg" />;
+    return (
+      <div className="cover-bg-box">
+        <div className="cover-bg" />
+      </div>
+    );
   }
 }
--- src/components/Cover/Main/Main.jsx
+++ src/components/Cover/Main/Main.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 export default class Main extends React.Component {
   render() {
-    return <main className="main container">h1</main>;
+    return (
+      <main className="main container">
+        <div className="main-title">
+          <h1>I'm not only</h1>
+          <p>
+            Ginger<span>Piranya</span>
+          </p>
+        </div>
+      </main>
+    );
   }
 }
