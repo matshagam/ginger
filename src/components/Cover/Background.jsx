edab58d56628720bdb777e9ff631f4d529212ebe
Dimitri A. Matveev <dimitri.matveev@icloud.com>
2018-08-06 16:39:14 +0300

    Попеременное появление элементов навигации

--- src/components/Cover/Background.jsx
+++ src/components/Cover/Background.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-export default class Background extends React.Component {
-  render() {
-    return (
-      <div className="cover-bg-box">
-        <div className="cover-bg" />
-      </div>
-    );
-  }
-}
+export const Background = () => {
+  return (
+    <div className="cover-bg-box">
+      <div className="cover-bg" />
+    </div>
+  );
+};
--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+
 import Header from './Header/Header.jsx';
 import { Main } from './Main/Main.jsx';
-import Background from './Background.jsx';
+import { Background } from './Background.jsx';
 
 export default class Cover extends React.Component {
   constructor(props) {
